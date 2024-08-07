ae20847dd38d83f04bf5d0827118aa822bff6335
zeyad Hany <theziadhany3094@gmail.com>
2024-08-07 16:38:54 +0300

    translated
     a pack to make localization

--- bookingsite/src/Components/hero.tsx
+++ bookingsite/src/Components/hero.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 import Nav from "@/Components/Nav";
 import Words from "@/Components/text";
 import './hero.css';
+import { useTranslation } from 'react-i18next';
 
 function Hero () {
+  const [t, i18next]= useTranslation();
   return (
     
     <div className='hero'> 
--- bookingsite/src/Components/text.tsx
+++ bookingsite/src/Components/text.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './text.css';
+import { useTranslation } from 'react-i18next';
+import '../i18n';
 
 function words () {
-     
+  const { t, i18n } = useTranslation();
   return (
     
     <div className='main'>
         <h1 className='hh'>
-        A Piece Of Paradise
-        Just For You
+        {t ('hh')}
         </h1>
         {/* <p className='para'> Best message coaches, best prices, best locations.
 
